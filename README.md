5cc3176c98ee6b63f1f5f044f2d9815d293e9bce
Jan Macario <jmacario@gmu.edu>
2022-08-13 19:09:51 -0400

    Adds basic plugin updating code.

--- README.md
+++
@@ -1,1 +0,0 @@
-# gmuw-wordpress-plugin-test-repo-20220813-01
---
+++ gmuw-wordpress-plugin-test-repo-20220813-01.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Test plugin file
+ */
+
+/**
+ * Plugin Name:       Mason WordPress: Test Plugin 20220813-01
+ * Author:            Mason Web Administration
+ * Plugin URI:        https://github.com/mason-webmaster/gmuw-wordpress-plugin-test-repo-20220813-01
+ * Description:       Test Plugin 20220813-01
+ * Version:           0.9
+ */
+
+
+// Exit if this file is not called directly.
+if (!defined('WPINC')) {
+	die;
+}
+
+// Set up auto-updates
+require 'plugin-update-checker/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+'https://github.com/mason-webmaster/gmuw-wordpress-plugin-test-repo-20220813-01',
+__FILE__,
+'gmuw-wordpress-plugin-test-repo-20220813-01'
+);
+
+// Plugin code...
